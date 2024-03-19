8aa668629ff32336dcfc1cbad87058292c7b00e4
JiSeongHah <jiseong1234@yonsei.ac.kr>
2024-03-19 20:56:38 +0900

    basic concepts

---
+++ newDataVer0/scripts/Dataload.py
@@ -0,0 +1,98 @@
+import os
+import yaml
+from pprint import pprint
+from torchvision import datasets,transforms
+import torch
+
+def readConfig(yaml_path):
+    
+    with open(yaml_path) as f:
+        loadedConfig = yaml.load(f, Loader=yaml.FullLoader)
+    
+    print('loaded config is :')
+    pprint(loadedConfig)    
+    
+    return loadedConfig
+
+
+
+def loadData(configs,isTrain):
+    
+    dataDict= {
+        'mnist',
+        'cifar10',
+        'mvtecad',
+        'stl10',
+        'crwu',
+    }
+    
+    whichData = configs['data_type']
+    dataDownPath = os.path.join(configs['data_down_path'],whichData)
+    
+    os.makedirs(dataDownPath,exist_ok=True)
+    
+    if whichData == 'mnist':
+        loadedData = datasets.MNIST(
+            root = dataDownPath,
+            train= isTrain,
+            download=True,
+            transform=transforms.ToTensor()
+        )
+        
+        return loadedData
+        
+    elif whichData == 'cifar10':
+        loadedData = datasets.CIFAR10(
+            root = dataDownPath,
+            train= isTrain,
+            download=True,
+            transform=transforms.ToTensor()
+        )
+        
+        return loadedData
+    
+    else:
+        pass
+        
+        
+    
+    
+class myNewDataset(torch.utils.data.Dataset): 
+    def __init__(self,configs,isTrain):
+        
+        self.configs= configs
+        self.isTrain = isTrain
+        self.doFlatten = configs['do_flatten']
+        
+        self.loadedData= loadData(
+            configs=configs,
+            isTrain=isTrain
+            )
+        
+
+    def __len__(self):
+        return len(self.loadedData)
+
+    def __getitem__(self, idx):
+        
+        data = self.loadedData[idx][0]
+        
+        if self.doFlatten:
+            data= torch.flatten(data)
+        
+        label = self.loadedData[idx][1]
+        
+        return data,label
+
+
+
+    
+yamlPath = './config.yaml'
+
+loadedYaml = readConfig(yamlPath)
+
+testDataset = myNewDataset(configs=loadedYaml,isTrain=True)
+import time
+for i in testDataset:
+    print(i[0].size(),i[1])
+    time.sleep(1)
